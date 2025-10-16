e386fe7fa0a85032cc429cc8311b09d8c2063456
PienkDrakie <pienkdrakie@gmail.com>
2025-10-16 14:46:06 +0200

    mapped out file. made simple makefile

---
+++ PracticeProblems/NumIsla/num_isla.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int num_isla();
+
+int main(int argc, char *argv[])
+{
+	/* read in grid from file and init m x n
+	 * call function to get num of islands
+	 * print result
+	 */
+
+	return EXIT_SUCCESS;
+}
+
+int num_isla()
+{
+	/* Algorithm:
+	* - loop through grid
+	* - if '1', then island
+	* - use DFS to remove island, '1'->'0'
+	* - continue
+	*/
+
+	int n = 0;
+
+	return n;
+}
