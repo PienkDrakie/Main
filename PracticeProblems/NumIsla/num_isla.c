040d8f6f2b2ba0bd8ecf65c336e944d6a18f7f5a
PienkDrakie <pienkdrakie@gmail.com>
2025-10-16 16:22:01 +0200

    Progress

--- PracticeProblems/NumIsla/num_isla.c
+++ PracticeProblems/NumIsla/num_isla.c
@@ -9,7 +9,25 @@ int main(int argc, char *argv[])
 	 * call function to get num of islands
 	 * print result
 	 */
+	int m, n, c, *grid;
+	FILE *fptr, *fsize;
 
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %c <filename>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	if ((fptr = fopen(argv[1], "r")) == NULL ) {
+		fprintf(stderr, "Unable to open file: \"%s\"\n", argv[1]);
+		return EXIT_FAILURE;
+	}
+	/* create a copy of the pointer to get m & n */
+	fsize = fptr;
+	while (fsize) {
+		// TODO: count num of rows and cols
+	}
+
+	fclose(fptr);
 	return EXIT_SUCCESS;
 }
 
