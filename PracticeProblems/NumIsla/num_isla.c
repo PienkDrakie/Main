72a7b6116d42c37910d10650c97c986b954d53ab
PienkDrakie <pienkdrakie@gmail.com>
2025-10-17 12:35:29 +0200

    Can now read in grids from files correctly.

--- PracticeProblems/NumIsla/num_isla.c
+++ PracticeProblems/NumIsla/num_isla.c
@@ -9,24 +9,78 @@ int main(int argc, char *argv[])
 	 * call function to get num of islands
 	 * print result
 	 */
-	int m, n, c, *grid;
-	FILE *fptr, *fsize;
+	int i, j, m, n, *nums, **grid;
+	int first_line, nmpty;
+	char c;
+	FILE *fptr;
+	m = 0;
+	n = 0;
+	first_line = 1;
+	nmpty = 0;
 
 	if (argc != 2) {
-		fprintf(stderr, "Usage: %c <filename>\n", argv[0]);
+		fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
 		return EXIT_FAILURE;
 	}
 
 	if ((fptr = fopen(argv[1], "r")) == NULL ) {
-		fprintf(stderr, "Unable to open file: \"%s\"\n", argv[1]);
+		fprintf(stderr, "ERROR: Unable to open file: \"%s\"\n", argv[1]);
 		return EXIT_FAILURE;
 	}
-	/* create a copy of the pointer to get m & n */
-	fsize = fptr;
-	while (fsize) {
-		// TODO: count num of rows and cols
+	i = 0;
+	j = 0;
+	/* get m x n dimensions (only need to count columns once) */
+	while ((c = getc(fptr)) != EOF) {
+		if (c == '1' || c == '0') {
+			if (first_line) j++;
+			nmpty = 1;
+		} else if (c == '\n') {
+			if (nmpty) i++;
+			nmpty = 0;
+			first_line = 0;
+		}
+	}
+	/* in case last line doesn't end with newline */
+	if (nmpty) i++;
+	m = i;
+	n = j;
+	rewind(fptr);
+
+	/* allocate memory for numbers and array of pointers(grid) */
+	if ((nums = malloc(m * n * sizeof(int))) == NULL) {
+		fprintf(stderr, "ERROR: Memory allocation failure.\n");
+		return EXIT_FAILURE;
+	}
+	if ((grid = malloc(m * sizeof(int *))) == NULL) {
+		fprintf(stderr, "ERROR: Memory allocation failure.\n");
+		free(nums);
+		return EXIT_FAILURE;
 	}
 
+	/* setup pointers to point at start of each row */
+	for (i = 0; i < m; i++) {
+		grid[i] = nums + (i * n);
+	}
+
+	printf("m: %d n: %d\n", m, n);
+	/* read in grid */
+	for (i = 0; i < m; i++) {
+		for (j = 0; j < n; j++) {
+			if (fscanf(fptr, "%d", &grid[i][j]) != 1) {
+				fprintf(stderr, "ERROR: Invalid grid data.\n");
+				free(nums);
+				free(grid);
+				fclose(fptr);
+				return EXIT_FAILURE;
+			}
+			printf("%d ", grid[i][j]);
+		}
+		printf("\n");
+	}
+
+	/* cleanup */
+	free(nums);
+	free(grid);
 	fclose(fptr);
 	return EXIT_SUCCESS;
 }
